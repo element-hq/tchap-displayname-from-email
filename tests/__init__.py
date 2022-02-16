3c5faa1fd53ca71ea0f346191bc6728e164e4546
Brendan Abolivier <babolivier@matrix.org>
2022-02-16 18:10:20 +0000

    Lint

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Any, Dict, Optional
-
 from unittest.mock import Mock
+
 from synapse.module_api import ModuleApi
 
 from tchap_username_email import TchapUsernameEmail
